222ae206e68eaa7589617fdf544dad2acc09cf3a
yuntaobc <yuntaobc@gmail.com>
2020-12-24 22:37:40 +0800

    bootstrap things

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,8 +4,11 @@ from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    # return render_template('404.html'), 404
+    return "404"
+
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    # return render_template('500.html'), 500
+    return "500"
