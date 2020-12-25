95265e4e4ae9ad67c281f70b7d727d7abac2afc1
yuntaobc <yuntaobc@gmail.com>
2020-12-25 13:11:48 +0800

    Change api prefix

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,6 @@ def create_app(config_name):
     from .api import api as api_blueprint
 
     app.register_blueprint(main_blueprint)
-    app.register_blueprint(api_blueprint, url_prefix='')
+    app.register_blueprint(api_blueprint, url_prefix='/api')
 
     return app
