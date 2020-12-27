31b00bfd53843fc40cd40d17ebe5216eed93d17a
yuntaobc <yuntaobc@gmail.com>
2020-12-27 09:58:59 +0800

    Add Datetimepicker and adjust fonts style

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,12 +4,19 @@ from wtforms import StringField, DateTimeField, SubmitField
 
 class EventUserForm(FlaskForm):
     name = StringField("Event name to search",
-                       render_kw={'oninput': 'event_list()',
-                                  'placeholder': 'Event name to search',
-                                  'id': 'name-input',
-                                  'class': 'typeahead',
-                                  'autocomplate': 'off',
-                                  'data-provide': 'typeahead'})
-    s_time = DateTimeField(format='%Y-%m-%dT%H:%M:%S')
-    e_time = DateTimeField(format='%Y-%m-%dT%H:%M:%S')
+                       render_kw={
+                           'oninput': 'event_list()',
+                           'placeholder': 'Event name to search',
+                           'id': 'name-input',
+                           'class': 'typeahead',
+                           'autocomplate': 'off',
+                           'data-provide': 'typeahead'})
+    s_time = DateTimeField(format='%Y-%m-%dT%H:%M:%S',
+                           render_kw={
+                               'id': 'datetimepicker1'
+                           })
+    e_time = DateTimeField(format='%Y-%m-%dT%H:%M:%S',
+                           render_kw={
+                               'id': 'datetimepicker2'
+                           })
     submit = SubmitField("Search")
